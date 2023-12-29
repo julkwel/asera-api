f52b905b7214bfdb34902aaea674c81f2aef0353
julkwel <jrajerison@bocasay.com>
2023-12-29 09:02:46 +0300

    Init env setup

--- api/tests/Api/GreetingsTest.php
+++ api/tests/Api/GreetingsTest.php
@@ -3,9 +3,21 @@
 namespace App\Tests\Api;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GreetingsTest extends ApiTestCase
 {
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     public function testCreateGreeting(): void
     {
         static::createClient()->request('POST', '/greetings', [
